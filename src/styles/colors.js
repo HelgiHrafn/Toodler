b86a60a8b84720c7a41c62267b5baf43f57c9347
HelgiHrafn <helgia21@ru.is>
2022-11-21 12:36:31 +0000

    colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,7 +1,16 @@
 export const sexualBlue = '#284B63';
-/* CSS HEX */
+
+
+/* Possible pallette 1: */
 export const uranianBlue = "#a3d9ffff";
 export const chineseViolet ="#7e6b8fff";
 export const celadon = "#96e6b3ff";
 export const amaranth = "#da3e52ff";
 export const corn = "#f2e94eff";
+
+/* Possible pallete 2: */
+export const fireOpal = "#ee6352ff";
+export const emerald = "#59cd90ff";
+export const ceruleanCrayola =  "#3fa7d6ff";
+export const maximumYellowRed = "#fac05eff";
+export const vividTangerine = "#f79d84ff";
