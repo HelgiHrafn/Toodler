cce558cbb0bcc877af481ec42ca10a271e34e09e
HelgiHrafn <helgia21@ru.is>
2022-11-21 12:28:26 +0000

    fix

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,1 +1,7 @@
 export const sexualBlue = '#284B63';
+/* CSS HEX */
+export const uranianBlue = "#a3d9ffff";
+export const chineseViolet ="#7e6b8fff";
+export const celadon = "#96e6b3ff";
+export const amaranth = "#da3e52ff";
+export const corn = "#f2e94eff";
