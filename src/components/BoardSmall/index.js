3aeafa372489a7cdddf4a5da7345c94a0ebe47ea
HelgiHrafn <helgia21@ru.is>
2022-11-22 13:32:56 +0000

    fullt af stöffi

---
+++ src/components/BoardSmall/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Image, View, Text } from 'react-native';
+import styles from './styles';
+
+const BoardSmall = ({ name, thumbnailPhoto }) => {
+    return (
+        <View style={[styles.boardSmall, styles.coolShadow]}>
+        <Text style={styles.name}>{name}</Text>
+        <Image
+            style={styles.image}
+            resizeMode="cover"
+            source={{uri: thumbnailPhoto}}
+        />
+
+    </View>
+    );
+    
+};
+
+export default BoardSmall;
