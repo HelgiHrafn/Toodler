50e4fd0ddf3f1512fabb3726afd265e6873dfd09
HelgiHrafn <helgia21@ru.is>
2022-11-22 15:04:04 +0000

    stuff

--- src/components/BoardList/styles.js
+++ src/components/BoardList/styles.js
@@ -2,14 +2,13 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
   boardList: {
-    marginHorizontal: 10,
     justifyContent: 'center',
-    height: '100%'
+    height: '100%',
   },
   h1: {
     fontSize: 40,
     fontFamily: 'Futura',
-    padding: 10
+    margin: 10
   }
 
 })
---
+++ src/views/SingleBoard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import data from '../../resources/data.json'
+
+const Board = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+export default Board
