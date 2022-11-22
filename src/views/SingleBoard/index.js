209b3d12e8f0adc2096076fdcfa575681ca9080b
HelgiHrafn <helgia21@ru.is>
2022-11-22 16:07:28 +0000

    stuff

--- src/views/SingleBoard/index.js
+++ src/views/SingleBoard/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import data from '../../resources/data.json'
 
-const Board = () => {
-    return (
+const Board = ({[ board, lists]}) => {
+    
         <View>
+            <Text>{board.name}</Text>
+            <FlatList
+                numColumns={1}
+                data={lists}
+                renderItem={({item:{name, color}}) => {
+                    return (
+                        <TaskList name={name} color={color}/>
+                    )
 
+                }}
+                keyExtractor={list => list.id}
+            />
         </View>
-    )
+    
 }
 export default Board
