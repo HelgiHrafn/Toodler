952edfd72887dd6032a3f73b1b6c490d9855583a
Gudmundur Kristjan <gummigudna@gmail.com>
2022-11-23 11:13:31 +0000

    routes + logo

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Boards from '../views/Main';
+import Board from '../views/SingleBoard';
+
+const Stack = createStackNavigator();
+
+const Routes = () => (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName='Boards'>
+            <Stack.Screen name ="Boards" component={Boards} />
+            <Stack.Screen name ="Board" component={Board} />
+        </Stack.Navigator>
+    </NavigationContainer>
+);
+
+export default Routes;
