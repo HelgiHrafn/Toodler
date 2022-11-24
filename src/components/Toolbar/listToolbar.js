510e9d345b43e1ee6cb80ade3337dc840b4a02d4
huxjoeflow <huxjoeflow@gmail.com>
2022-11-24 13:03:23 +0000

    select lists

--- src/components/Toolbar/listToolbar.js
+++ src/components/Toolbar/listToolbar.js
@@ -3,14 +3,16 @@ import { View, TouchableHighlight, Text, Image} from 'react-native'
 import logo from '../../resources/logo.png'
 import styles from './styles'
 
-const ListToolbar = () => (
+const ListToolbar = ({ hasSelectedLists}) => (
     <View style={styles.toolbar}>
         <TouchableHighlight style={styles.toolbarAction}>
             <Text style={styles.toolbarActionText}>Create List</Text>
         </TouchableHighlight>
         <Image style={styles.logo} source={logo} />
-        <TouchableHighlight style={styles.toolbarAction}>
-            <Text style={styles.toolbarActionText}>Delete</Text>
+        <TouchableHighlight 
+            style={styles.toolbarAction}
+            disables={!hasSelectedLists}>
+            <Text style={[styles.toolbarActionText, { color: !hasSelectedLists ? 'gray' :{}}]}>Delete</Text>
         </TouchableHighlight>
 
     </View>
--- src/views/SingleBoard/index.js
+++ src/views/SingleBoard/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, FlatList } from 'react-native'
 import data from '../../resources/data.json'
 import styles from './styles'
@@ -6,22 +6,37 @@ import TaskList from '../../components/TaskList'
 import ListToolbar from '../../components/Toolbar/listToolbar'
 
 const Board = ({ navigation, route }) => {
-  const board = route.params.board
-  var lists = data.lists
-  lists = lists.filter(function(element) {return element.boardId == board.id})
+  const board = route.params.board;
+  var lists = data.lists;
+  lists = lists.filter(function(element) {return element.boardId == board.id});
+  // All lists within the board
+  const [boardList, setBoardList] = useState(lists);
+  //All selected lists from said board
+  const [selectedBoardList, setSelectedBoardList] = useState([]);
+
+  const onBoardListLongPress = name => {
+     if (selectedBoardList.indexOf(name) !== -1) {
+       setSelectedBoardList(selectedBoardList.filter(boardList => boardList !== name ))
+     } else {
+       setSelectedBoardList([...selectedBoardList, name]);
+     }
+  }; 
   return (
         <View style={styles.main}>
-        <ListToolbar />
+        <ListToolbar hasSelectedLists={selectedBoardList.length > 0 } />
         <View style={[styles.boardBig, styles.coolShadow]}>
 
             <Text style={styles.h2}>{board.name}</Text>
             <FlatList
                 numColumns={1}
-                data={lists}
+                data={boardList}
                 renderItem={({ item }) => {
                   return (
-
-                        <TaskList list={item} navigation={navigation}/>
+                        <TaskList 
+                        list={item}
+                        onLongPress={name => onBoardListLongPress(name)}
+                        isSelected={selectedBoardList.indexOf(item.name) !==-1}
+                        navigation={navigation}/>
                   )
                 }}
                 keyExtractor={list => list.id}
