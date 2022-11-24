52d94070745fe8a17ed92f169124a186ed8beccc
Gudmundur Kristjan <gummigudna@gmail.com>
2022-11-24 13:17:15 +0000

    checkbox phone fix

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableHighlight, Switch, CheckBox } from 'react-native'
+import { View, Text, TouchableHighlight, Switch } from 'react-native'
+import CheckBox from 'expo-checkbox';
 import styles from './styles'
 
 const Task = ({ task, navigation }) => {
-  const [check, setCheck] = useState(task.isFinished);
+  const [toggleCheckBox, setToggleCheckBox] = useState(task.isFinished)
 
   return (
     <TouchableHighlight style={styles.touchable}>
     <View style={styles.list}>
       {/* <input type="checkbox" style={styles.dot} checked={task.isFinished}/> */}
       {/* <Switch style={styles.checkbox} value={check} onValueChange={value => setCheck(value)} /> */}
-      <CheckBox style={styles.checkbox} value={check} onValueChange={value => setCheck(value)} />
+      <CheckBox style={styles.checkbox} value={toggleCheckBox} onValueChange={newValue => setToggleCheckBox(newValue)} />
       <View style={styles.textwrapper}>
           <Text style={styles.h3}>{task.name}</Text>
           <Text style={styles.h4}>{task.description}</Text>
