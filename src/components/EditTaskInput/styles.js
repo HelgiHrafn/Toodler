71a91c2e8ee1ae9cb7e8c9c2f766f0c01e4aaf27
huxjoeflow <huxjoeflow@gmail.com>
2022-11-25 19:15:26 +0000

    fix

---
+++ src/components/EditTaskInput/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Dimensions } from 'react-native'
+import { emerald } from '../../styles/colors'
+const { width: winWidth } = Dimensions.get('window')
+
+export default StyleSheet.create({
+  input: {
+    fontFamily: 'Futura',
+    fontSize: 20,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 7,
+    margin: 2,
+    padding: 2,
+    width: winWidth - 120
+  },
+  button: {
+    fontFamily: 'Futura',
+    fontSize: 20
+  },
+  buttonBackground: {
+    // backgroundColor: emerald,
+    width: '100%',
+    alignSelf: 'flex-end',
+    padding: 4,
+    marginBottom: 5,
+    marginTop: 10,
+    borderRadius: 7
+  }
+})
