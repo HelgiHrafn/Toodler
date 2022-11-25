2e0364071c19e9de204f7154b75315c4a7b92526
Gudmundur Kristjan <gummigudna@gmail.com>
2022-11-25 18:33:10 +0000

    color 0 banned

--- src/components/CreateListInput/index.js
+++ src/components/CreateListInput/index.js
@@ -29,15 +29,15 @@ const CreateListInput = ({ addList, closeModal }) => {
     if (!name) {
       errors.name = 'Name can not be empty'
     }
-    if (color === '') {
+    if (color === '' || color === '0') {
       errors.color = 'Please select a color from the dropdown menu'
     }
-
+    
     setErrors(errors)
 
     return Object.keys(errors).length > 0 ? false : true
   }
-
+  
   const register = () => {
     if (validateForm()) {
       addList(inputs)
