b338e029b8afacab4ec8459b428477ff7cbb3287
HelgiHrafn <helgia21@ru.is>
2022-11-25 12:09:02 +0000

    fix add list

--- src/components/CreateListInput/index.js
+++ src/components/CreateListInput/index.js
@@ -24,16 +24,17 @@ const CreateListInput = ({ addList, closeModal }) => {
                 placeholder="Name"
                 value={inputs.name}
                 onChangeText={text => inputHandler('name', text)} />
+            <Text>Choose color:</Text>
             <Picker
                 style={styles.picker}
                 selectedValue={inputs.color}
                 onValueChange={value => inputHandler('color', value)}>
                   
-                <Picker.Item label="fireOpal" value="#ee6352ff" />
-                <Picker.Item label="emerald" value="#59cd90ff" />
-                <Picker.Item label="ceruleanCrayola" value="#3fa7d6ff" />
-                <Picker.Item label="maximumYellowRed" value="#fac05eff" />
-                <Picker.Item label="vividTangerine" value="#f79d84ff" />
+                <Picker.Item label="Fire opal" value="#ee6352ff" />
+                <Picker.Item label="Emerald green" value="#59cd90ff" />
+                <Picker.Item label="Cerulean crayola" value="#3fa7d6ff" />
+                <Picker.Item label="Maximum Yellow Red" value="#fac05eff" />
+                <Picker.Item label="Vivid Tangerine" value="#f79d84ff" />
             </Picker>
 
             <TouchableOpacity
